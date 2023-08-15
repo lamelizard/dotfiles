17dcb13dfc8529f59ca822971241934a04d2138a
lamelizard <t.eddenburg@gmail.com>
2023-08-15 23:34:26 +0200

    WSL git credentials

--- .scripts/bash/setup/basics_cli.sh
+++ .scripts/bash/setup/basics_cli.sh
@@ -16,9 +16,19 @@ ln -fs $(which fdfind) ~/.bin/fd
 
 # git with credential store
 apt-get --no-install-recommends install -y \
-  git \
-  libsecret-1-0 \
-  libsecret-1-dev
-make -C /usr/share/doc/git/contrib/credential/libsecret
-git config --global credential.helper \
-  /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
+  git
+# are we in WSL?
+# https://stackoverflow.com/questions/38859145/detect-ubuntu-on-windows-vs-native-ubuntu-from-bash-script
+if grep -qi microsoft /proc/version; then
+  # WSL -> use Git for windows, rather hard coded...
+  echo "WSL: using Git for Windows credential manager"
+  git config --global credential.helper \
+    "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe"
+else
+  apt-get --no-install-recommends install -y \
+    libsecret-1-0 \
+    libsecret-1-dev
+  make -C /usr/share/doc/git/contrib/credential/libsecret
+  git config --global credential.helper \
+    /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
+fi
