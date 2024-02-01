fea46fa602f17d8739a92440cde9133dc0988ab5
lamelizard <lamelizard@users.noreply.github.com>
2024-02-01 21:24:56 +0100

    lualine remove broken separators

--- .config/nvim/lua/lamelizard/lualine.lua
+++ .config/nvim/lua/lamelizard/lualine.lua
@@ -2,6 +2,9 @@ local lualine = require('lualine')
 return lualine.setup({
   options = {
   icons_enabled = false, -- needs patched font
+  -- default seperators not available everythere
+  section_separators = '',
+  component_separators = '',
   --theme = 'auto',
   },
   --extensions = {
