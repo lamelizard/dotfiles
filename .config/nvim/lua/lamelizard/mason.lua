b40e457a100aeb12bc14a9449eae8ff46f7eccb7
lamelizard <lamelizard@users.noreply.github.com>
2024-11-16 14:09:40 +0100

    Mason allow install from shell

--- .config/nvim/lua/lamelizard/mason.lua
+++ .config/nvim/lua/lamelizard/mason.lua
@@ -1,7 +1,15 @@
+local ensure_installed = {
+  "lua_ls",
+  "texlab",
+}
+
+-- to install from shell
+vim.api.nvim_create_user_command("MasonInstallAll", function ()
+  vim.cmd("MasonInstall " .. table.concat(ensure_installed, " "))
+end, {})
+
 require("mason").setup()
 require("mason-lspconfig").setup(
-  ensure_installed = {
-    "texlab",
-  }
+  ensure_installed = ensure_installed
 )
 
