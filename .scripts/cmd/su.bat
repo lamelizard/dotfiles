4e664b364e0d22d70864cd9b22a901f11744b135
lamelizard <lamelizard@users.noreply.github.com>
2024-11-16 11:17:48 +0100

    su.bat - elevate cmd (wt) to admin

---
+++ .scripts/cmd/su.bat
@@ -0,0 +1,10 @@
+@echo off
+:: Check if running with administrative privileges
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    echo This Command Prompt is already running as administrator.
+) else (
+    echo Opening a new Windows Terminal as administrator...
+    powershell -Command "Start-Process 'wt.exe' -ArgumentList 'cmd /k cd %CD%' -Verb RunAs"
+    exit
+)
