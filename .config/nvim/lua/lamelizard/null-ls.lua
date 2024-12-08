132c2b0ca9b34e688dd1f7dde0d82b3b15257d8a
lamelizard <lamelizard@users.noreply.github.com>
2024-12-08 18:32:52 +0100

    null-ls remove deprecated chktex...

--- .config/nvim/lua/lamelizard/null-ls.lua
+++ .config/nvim/lua/lamelizard/null-ls.lua
@@ -2,7 +2,9 @@ local nls = require('null-ls')
 
 local selected_sources = {
   nls.builtins.formatting.shfmt, -- shell script formatting
-  nls.builtins.diagnostics.chktex, -- latex linting
+  --got deprecated...
+  --https://github.com/nvimtools/none-ls.nvim/issues/58
+  --nls.builtins.diagnostics.chktex, -- latex linting
 }
 
 require('null-ls').setup({
