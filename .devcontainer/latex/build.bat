3ee6d91d553d8a2be04430c688122f721bdd9778
lamelizard <lamelizard@users.noreply.github.com>
2025-01-18 17:36:41 +0100

    latex-gui devcontainer

--- .devcontainer/latex/build.bat
+++ .devcontainer/latex/build.bat
@@ -1,12 +1,23 @@
 @echo off
 
-REM Build the Docker image using the Dockerfile in the current directory
-docker build -t "latex" --build-arg BREAK_CACHE="%time%" .
+REM break the cache, but only once for all images together
+SET CURRENT_TIME="%time%"
 
+REM Build the Docker images using the Dockerfile in the current directory
+docker build --target latex -t "latex" --build-arg BREAK_CACHE="%CURRENT_TIME%" .
 REM Check if the build was successful
-IF %ERRORLEVEL% EQU 0 (
-    echo Docker image built successfully: %ImageName%
-) ELSE (
+IF %ERRORLEVEL% NEQ 0 (
     echo Failed to build Docker image.
+    pause
+    exit /b %ERRORLEVEL%
 )
+echo Docker image built successfully: %ImageName%
+
+docker build --target latex-gui -t "latex-gui" --build-arg BREAK_CACHE="%CURRENT_TIME%" .
+IF %ERRORLEVEL% NEQ 0 (
+    echo Failed to build Docker image.
+    pause
+    exit /b %ERRORLEVEL%
+)
+echo Docker image built successfully: %ImageName%
 pause
