4cfa808bda9cae8c5c8c65021007982878475cbb
lamelizard <lamelizard@users.noreply.github.com>
2025-01-26 23:05:07 +0100

    dotfiles.bat open lazygit if no arguments

--- .scripts/cmd/dotfiles.bat
+++ .scripts/cmd/dotfiles.bat
@@ -1,7 +1,12 @@
 @echo off
 :: .bat instead of an bash alias
 if "%~1" == "" (
-  echo Usage: %~nx0 arguments & exit /b 1
+  where lazygit >nul 2>&1
+  if %errorlevel% == 0 (
+    lazygit --git-dir="%USERPROFILE%\.dotfiles" --work-tree="%USERPROFILE%"
+  ) else (
+    echo Usage: %~nx0 arguments (or have lazygit installed) & exit /b 1
+  )
 )
 
 git --git-dir="%USERPROFILE%\.dotfiles" --work-tree="%USERPROFILE%" %*
