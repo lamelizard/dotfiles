f89b320420fd1b4f0277be6355fedf55cbabb07c
lamelizard <lamelizard@users.noreply.github.com>
2025-06-01 17:03:25 +0200

    install more silently

--- .config/nvim/install_win.bat
+++ .config/nvim/install_win.bat
@@ -1,6 +1,6 @@
 @echo off
 REM set NVIM_UNIX_DIR="%USERPROFILE%\.config\nvim\"
 REM set NVIM_WIN_DIR="%LOCALAPPDATA%\nvim\"
-@echo on
 REM xcopy %NVIM_UNIX_DIR% %NVIM_WIN_DIR% /S /K /D /Y
+@echo on
 mklink /D "%USERPROFILE%\AppData\Local\nvim" "%USERPROFILE%\.config\nvim"
