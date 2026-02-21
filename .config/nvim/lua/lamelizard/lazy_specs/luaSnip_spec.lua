7705a35e8987e2029952aa9e5bd3035a20f2b56b
lamelizard <lamelizard@users.noreply.github.com>
2026-02-21 13:08:10 +0100

    TreeSitter fix and add LuaSnip

--- .config/nvim/lua/lamelizard/lazy_specs/luaSnip_spec.lua
+++ .config/nvim/lua/lamelizard/lazy_specs/luaSnip_spec.lua
@@ -1,10 +1,7 @@
 return {
-  -- better search f,t
   'L3MON4D3/LuaSnip',
   version = "v2.*",
-  -- not really a dependency, but fits here
-  dependencies = {
-    'rafamadriz/friendly-snippets'
-  },
+  config = function() require 'lamelizard.luasnip' end
+  ,
 }
 
--- .config/nvim/lua/lamelizard/lazy_specs/treesitter_spec.lua
+++ .config/nvim/lua/lamelizard/lazy_specs/treesitter_spec.lua
@@ -1,5 +1,7 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  -- take the latest release version
+  version = '*',
   build = ':TSUpdate',
   config = function() require 'lamelizard.treesitter' end,
 }
